fd02b9574cc266f66020e42fa226daa8ca77a591
Ruhui Wen <ruhuiwen0608@gmail.com>
2023-03-25 22:36:50 +0200

    add routers: count journeys start from a station and return from a station

--- server/controllers/journeyRouter.js
+++ server/controllers/journeyRouter.js
@@ -11,10 +11,17 @@ journeyRouter.get('/', async (request, response) => {
 journeyRouter.get('/departureFrom/:departureStation', (request, response) => {
     Journey.find({ Departure_station_id: request.params.departureStation }).then(journeys => response.json(journeys));
 })
+journeyRouter.get('/departureFrom/:departureStation/count', (request, response) => {
+    Journey.find({ Departure_station_id: request.params.departureStation }).count().then(journeys => response.json(journeys))
+})
 journeyRouter.get('/departureFrom/:departureStation/aggregation', aggregation.journeyAggrByDepartureStation)
+
 
 journeyRouter.get('/returnFrom/:returnStation', (request, response) => {
     Journey.find({ Return_station_id: request.params.returnStation }).then(journeys => response.json(journeys));
+})
+journeyRouter.get('/returnFrom/:returnStation/count', (request, response) => {
+    Journey.find({ Return_station_id: request.params.returnStation }).count().then(journeys => response.json(journeys))
 })
 journeyRouter.get('/returnFrom/:returnStation/aggregation', aggregation.journeyAggrByReturnStation)
 module.exports = journeyRouter;
