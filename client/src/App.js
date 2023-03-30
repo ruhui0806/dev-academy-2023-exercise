6a00776bd40f78b97b5d89da4362afc824087753
Ruhui Wen <ruhuiwen0608@gmail.com>
2023-03-30 17:04:17 +0300

    add journey router and journeys page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
 import Stations from './pages/Stations';
+import Journeys from './pages/Journeys';
 import StationView from './pages/StationView';
 
 
@@ -11,12 +12,17 @@ const App = () => {
     <div>
       <h1>hello world</h1>
       <Router>
+        <div>
+          <Link to="/stations" id="stations">Stations</Link>
+          <Link to="/journeys" id="journeys">Journeys</Link>
+        </div>
         <Routes>
           <Route path="/stations" element={<Stations />} />
           <Route
             path="/stations/:ID"
             element={<StationView />}
           />
+          <Route path="/journeys" element={<Journeys />} />
         </Routes>
       </Router>
     </div>
