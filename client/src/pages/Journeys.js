73ff22dee97f6e4adb6c3577cd74eccd7f5de8b1
文静 <ruhui@bunseis-MacBook-Pro.local>
2023-04-03 17:15:16 +0300

    commit after revert 5fa7e4de

--- client/src/pages/Journeys.js
+++ client/src/pages/Journeys.js
@@ -17,8 +17,6 @@ export default function Journeys() {
         direction: 'descending',
     });
     useEffect(() => {
-        // let orderByColumn = ['Return_station_name', 'descending']
-        //let orderByColumn = 'Departure_station_name,ascending';
         let direction = sortConfig.direction === 'ascending'? '' : '-';
         let orderByColumn = direction + sortConfig.attr;
         //offset = page*journeysPerPage, limit=journeysPerPage, order = orderByColumn
