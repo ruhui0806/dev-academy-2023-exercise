977d9d666f24bc08e9639d60024cefd0e46fa983
文静 <ruhui@bunseis-MacBook-Pro.local>
2023-04-14 13:10:15 +0300

    small changes on routers

--- server/controllers/journeyRouter.js
+++ server/controllers/journeyRouter.js
@@ -1,21 +1,25 @@
 const journeyRouter = require("express").Router();
 const Journey = require("../models/journey");
-const aggregation = require("../aggregation");
 
-journeyRouter.get("/", async (request, response) => {
-  const journeys = await Journey.find({
-    Covered_distance_m: { $gte: request.query.filterByDistance },
-    Duration_sec: { $gte: request.query.filterByDuration },
-  })
-    .sort(request.query.order)
-    .skip(Number(request.query.offset))
-    .limit(Number(request.query.limit));
+journeyRouter.get("/", async (request, response, next) => {
+  try {
+    const journeys = await Journey.find({
+      Covered_distance_m: { $gte: request.query.filterByDistance },
+      Duration_sec: { $gte: request.query.filterByDuration },
+    })
+      .sort(request.query.order)
+      .skip(Number(request.query.offset))
+      .limit(Number(request.query.limit));
 
-  const journeysCount = await Journey.find({
-    Covered_distance_m: { $gte: request.query.filterByDistance },
-    Duration_sec: { $gte: request.query.filterByDuration },
-  }).count();
-  response.json({ journeys, journeysCount });
+    const journeysCount = await Journey.find({
+      Covered_distance_m: { $gte: request.query.filterByDistance },
+      Duration_sec: { $gte: request.query.filterByDuration },
+    }).count();
+    response.json({ journeys, journeysCount });
+    console.log(request.query);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = journeyRouter;
