2ed11790ae9350785e92e8fe9773b0e05fff9efb
文静 <ruhuiwen0608@gmail.com>
2023-04-17 11:44:38 +0300

    update error handling

--- server/controllers/journeyRouter.js
+++ server/controllers/journeyRouter.js
@@ -2,24 +2,20 @@ const journeyRouter = require("express").Router();
 const Journey = require("../models/journey");
 
 journeyRouter.get("/", async (request, response, next) => {
-  try {
-    const journeys = await Journey.find({
-      Covered_distance_m: { $gte: request.query.filterByDistance },
-      Duration_sec: { $gte: request.query.filterByDuration },
-    })
-      .sort(request.query.order)
-      .skip(Number(request.query.offset))
-      .limit(Number(request.query.limit));
+  const journeys = await Journey.find({
+    Covered_distance_m: { $gte: request.query.filterByDistance },
+    Duration_sec: { $gte: request.query.filterByDuration },
+  })
+    .sort(request.query.order)
+    .skip(Number(request.query.offset))
+    .limit(Number(request.query.limit));
 
-    const journeysCount = await Journey.find({
-      Covered_distance_m: { $gte: request.query.filterByDistance },
-      Duration_sec: { $gte: request.query.filterByDuration },
-    }).count();
-    response.json({ journeys, journeysCount });
-    console.log(request.query);
-  } catch (error) {
-    return next(error);
-  }
+  const journeysCount = await Journey.find({
+    Covered_distance_m: { $gte: request.query.filterByDistance },
+    Duration_sec: { $gte: request.query.filterByDuration },
+  }).count();
+  response.json({ journeys, journeysCount });
+  console.log(request.query);
 });
 
 module.exports = journeyRouter;
--- server/controllers/stationRouter.js
+++ server/controllers/stationRouter.js
@@ -2,74 +2,67 @@ const stationRouter = require("express").Router();
 const Station = require("../models/station");
 const Journey = require("../models/journey");
 
-stationRouter.get("/", (request, response) => {
-  Station.find({})
-    .then((stations) => response.json(stations))
-    .catch((err) => alert(err.message));
+stationRouter.get("/", async (request, response) => {
+  const stations = await Station.find({});
+  response.json(stations);
 });
-stationRouter.delete("/:ID", (request, response) => {
-  Station.findOneAndDelete({ ID: request.params.ID })
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((err) => next(err));
+
+stationRouter.delete("/:ID", async (request, response) => {
+  await Station.findOneAndDelete({ ID: request.params.ID });
+  response.status(204).end();
 });
 
 stationRouter.get("/:ID", async (request, response, next) => {
-  try {
-    const station = await Station.findOne({ ID: request.params.ID });
-    if (station) {
-      const countJourneyStartHere = await Journey.find({
-        Departure_station_id: request.params.ID,
-      }).count();
-      const countJourneyEndHere = await Journey.find({
-        Return_station_id: request.params.ID,
-      })
-        .count()
-        .count();
-      const aggrJourneyReturn = await Journey.aggregate(
-        AggrCountParams(
-          { Return_station_id: request.params.ID },
-          "$Departure_station_name",
-          -1,
-          5
-        )
-      );
-      const aggrJourneyDeparture = await Journey.aggregate(
-        AggrCountParams(
-          { Departure_station_id: request.params.ID },
-          "$Return_station_name",
-          -1,
-          5
-        )
-      );
-      const averageDepartunreDistance = await Journey.aggregate(
-        AverageParams(
-          { Departure_station_id: request.params.ID },
-          "$Departure_station_id"
-        )
-      );
-      const averageReturnDistance = await Journey.aggregate(
-        AverageParams(
-          { Return_station_id: request.params.ID },
-          "$Return_station_id"
-        )
-      );
+  const station = await Station.findOne({ ID: request.params.ID });
+  if (station) {
+    const countJourneyStartHere = await Journey.find({
+      Departure_station_id: request.params.ID,
+    }).count();
+    const countJourneyEndHere = await Journey.find({
+      Return_station_id: request.params.ID,
+    })
+      .count()
+      .count();
+    const aggrJourneyReturn = await Journey.aggregate(
+      AggrCountParams(
+        { Return_station_id: request.params.ID },
+        "$Departure_station_name",
+        -1,
+        5
+      )
+    );
+    const aggrJourneyDeparture = await Journey.aggregate(
+      AggrCountParams(
+        { Departure_station_id: request.params.ID },
+        "$Return_station_name",
+        -1,
+        5
+      )
+    );
+    const averageDepartunreDistance = await Journey.aggregate(
+      AverageParams(
+        { Departure_station_id: request.params.ID },
+        "$Departure_station_id"
+      )
+    );
+    const averageReturnDistance = await Journey.aggregate(
+      AverageParams(
+        { Return_station_id: request.params.ID },
+        "$Return_station_id"
+      )
+    );
 
-      response.json({
-        currentStation: station,
-        countJourneyStartHere: countJourneyStartHere,
-        countJourneyEndHere: countJourneyEndHere,
-        aggrJourneyReturn: aggrJourneyReturn,
-        aggrJourneyDeparture: aggrJourneyDeparture,
-        averageDepartunreDistance: averageDepartunreDistance,
-        averageReturnDistance: averageReturnDistance,
-      });
-    } else {
-      response.status(404).send({ error: "unknown endpoint" });
-    }
-  } catch (error) {
-    return next(error);
+    response.json({
+      currentStation: station,
+      countJourneyStartHere: countJourneyStartHere,
+      countJourneyEndHere: countJourneyEndHere,
+      aggrJourneyReturn: aggrJourneyReturn,
+      aggrJourneyDeparture: aggrJourneyDeparture,
+      averageDepartunreDistance: averageDepartunreDistance,
+      averageReturnDistance: averageReturnDistance,
+    });
+  } else {
+    response.status(404).send({ error: "unknown endpoint" });
   }
 });
 
