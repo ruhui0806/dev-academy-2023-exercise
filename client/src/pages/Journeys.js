07394c128ee89ff1952681b42e4b905067bd5add
文静 <ruhuiwen0608@gmail.com>
2023-04-22 00:35:47 +0300

    update the filter function in journey list page

--- client/src/pages/Journeys.js
+++ client/src/pages/Journeys.js
@@ -4,13 +4,15 @@ import JourneyRow from "../components/JourneyRow";
 import journeyService from "../services/journeys.js";
 import Pagination from "../components/pagination";
 import AddJourneyModal from "../components/AddJourneyModal";
-
+import { MdFilterAlt } from "react-icons/md";
 export default function Journeys() {
   const [journeys, setJourneys] = useState([]);
   const [page, setPage] = useState(0);
   const [journeysPerPage, setJourneysPerPage] = useState(10); //limit
   const [valueForFilterByDistance, setValueForFilterByDistance] = useState(0);
+  const [filterByDistanceInput, setFilterByDistanceInput] = useState(0);
   const [valueForFilterByDuration, setValueForFilterByDuration] = useState(0);
+  const [filterByDurationInput, setFilterByDurationInput] = useState(0);
   const [sortConfig, setSortConfig] = useState({
     attr: "Return_station_name",
     direction: "descending",
@@ -50,11 +52,10 @@ export default function Journeys() {
   };
   //component styles:
   const buttonStyle = {
-    marginTop: 5,
-    marginBottom: 5,
-    marginLeft: 5,
-    paddingLeft: 7,
-    paddingRight: 7,
+    marginTop: 4,
+    marginBottom: 4,
+    marginLeft: 8,
+    padding: 6,
     fontSize: 15,
   };
   const handleChangePage = (event, newPage) => {
@@ -68,15 +69,21 @@ export default function Journeys() {
   };
   const handleFilterByDistance = (event) => {
     event.preventDefault();
-    setValueForFilterByDistance(event.target.value * 1000);
+    setFilterByDistanceInput(event.target.value * 1000);
+  };
+  const handleFilterByDistanceChange = () => {
+    setValueForFilterByDistance(filterByDistanceInput);
     setPage(0);
   };
   const handleFilterByDuration = (event) => {
     event.preventDefault();
-    setValueForFilterByDuration(event.target.value * 60);
+    setFilterByDurationInput(event.target.value * 60);
     setPage(0);
   };
-
+  const handleFilterByDurationChange = () => {
+    setValueForFilterByDuration(filterByDurationInput);
+    setPage(0);
+  };
   const handleAddNewJourney = (object) => {
     journeyService.addJourney(object);
     setPage(0);
@@ -86,6 +93,7 @@ export default function Journeys() {
       .deleteJourneyById(id)
       .then(() => setJourneys(journeys.filter((j) => j._id !== id)));
   };
+
   return (
     <div id="journeys-page">
       <h3>Journeys</h3>
@@ -97,19 +105,35 @@ export default function Journeys() {
           <h5>Filter journey by covered distance (km) longer than:</h5>
           <input
             type="number"
-            id="valueForFilterByDistance"
+            min="0"
+            id="filterByDistanceInput"
             placeholder="Filter by distance"
             onChange={handleFilterByDistance}
           />
+          <button
+            onClick={handleFilterByDistanceChange}
+            style={buttonStyle}
+            className="button"
+          >
+            <MdFilterAlt />{" "}
+          </button>
         </div>
         <div className="journey-filter-box">
           <h5>Filter journey by duration (min) longer than:</h5>
           <input
             type="number"
-            id="valueForFilterByDuration"
+            min="0"
+            id="filterByDurationInput"
             placeholder="Filter by duration"
             onChange={handleFilterByDuration}
           />
+          <button
+            onClick={handleFilterByDurationChange}
+            style={buttonStyle}
+            className="button"
+          >
+            <MdFilterAlt />{" "}
+          </button>
         </div>
       </div>
 
